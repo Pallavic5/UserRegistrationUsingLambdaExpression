16253d11bdc8cadb83dc821cb260d2da6403d885
Pallavic5 <pchedge235@gmail.com>
2022-12-27 04:55:18 +0530

    Added: Logic of user need to enter a valid first name.

---
+++ src/com/bridgelabz/userregistrationusinglambda/UserRegistration.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.userregistrationusinglambda;
+/*
+ * Problem Statement UC1 As a User need to enter a valid First Name
+- First name starts with Cap and has
+minimum 3 characters
+ */
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+	
+	@FunctionalInterface
+	interface Validation {
+		public String validate(String regex, String userDetails);
+	}
+	/*
+	 * () -> {} (This is the Lambda Expression structure)
+	 */
+	public static void validFirstName() {
+		Validation isValidFirstName = (pattern, firstName) -> {
+			return "First Name is " + Pattern.compile(pattern).matcher(firstName).matches();
+		};
+		System.out.println(isValidFirstName.validate("^[A-Z]{1}[a-z]{2,}$", "Pallavi"));
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to User Registration Problem using lambda");
+		validFirstName();
+	}
+}
